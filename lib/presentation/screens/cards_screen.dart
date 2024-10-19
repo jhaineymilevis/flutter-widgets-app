fbc01ec08c2ca5da0889a416fcc9e3cba3721b1f
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-19 17:24:13 -0500

    creating image cards creating another card types

--- lib/presentation/screens/cards_screen.dart
+++ lib/presentation/screens/cards_screen.dart
@@ -7,9 +7,6 @@ const cards = <Map<String, dynamic>>[
   {'elevation': 3.0, 'label': 'Elevation 3'},
   {'elevation': 4.0, 'label': 'Elevation 4'},
   {'elevation': 5.0, 'label': 'Elevation 5'},
-  {'elevation': 6.0, 'label': 'Elevation 6'},
-  {'elevation': 7.0, 'label': 'Elevation 7'},
-  {'elevation': 8.0, 'label': 'Elevation 8'},
 ];
 
 class CardsScreen extends StatelessWidget {
@@ -35,6 +32,15 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cards.map((card) =>
               _CardType1(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType2(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType3(label: card['label'], elevation: card['elevation'])),
+          ...cards.map((card) =>
+              _CardType4(label: card['label'], elevation: card['elevation'])),
+          const SizedBox(
+            height: 50,
+          )
         ],
       ),
     );
@@ -65,6 +71,108 @@ class _CardType1 extends StatelessWidget {
               )
             ],
           )),
+    );
+  }
+}
+
+class _CardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          side: BorderSide(
+            color: colors.outline,
+          )),
+      elevation: elevation,
+      child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.more_vert_outlined))),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text('$label-outline'),
+              )
+            ],
+          )),
+    );
+  }
+}
+
+class _CardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      color: colors.surfaceContainerHighest,
+      elevation: elevation,
+      child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+          child: Column(
+            children: [
+              Align(
+                  alignment: Alignment.topRight,
+                  child: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.more_vert_outlined))),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Text('$label - Filled'),
+              )
+            ],
+          )),
+    );
+  }
+}
+
+class _CardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+  const _CardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/350',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius:
+                        BorderRadius.only(bottomLeft: Radius.circular(20))),
+                child: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(Icons.more_vert_outlined)),
+              )),
+        ],
+      ),
     );
   }
 }
