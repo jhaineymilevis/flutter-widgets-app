bee981824de1a3f5bae10869d8078e29658c9205
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-19 17:31:03 -0500

    filling barril file

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -3,3 +3,8 @@
 export 'package:flutter_application_1/presentation/screens/home_screen.dart';
 export 'package:flutter_application_1/presentation/screens/cards_screen.dart';
 export 'package:flutter_application_1/presentation/screens/buttons_screen.dart';
+export 'package:flutter_application_1/presentation/screens/animated_screen.dart';
+export 'package:flutter_application_1/presentation/screens/app_tutorial_screen.dart';
+export 'package:flutter_application_1/presentation/screens/infinite_scroll_screen.dart';
+export 'package:flutter_application_1/presentation/screens/snackbar_screen.dart';
+export 'package:flutter_application_1/presentation/screens/ui_controll_screen.dart';
