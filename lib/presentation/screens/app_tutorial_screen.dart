1152a8433b657b3ff82a3e4ca0089ee3bec5b4df
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-19 17:29:02 -0500

    creating another screens

---
+++ lib/presentation/screens/app_tutorial_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class AppTutorialScren extends StatelessWidget {
+  static const name = 'app_tutorial_screen';
+  const AppTutorialScren({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/ui_controll_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UiControllScreen extends StatelessWidget {
+  static const name = 'ui_controll_screen';
+  const UiControllScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
