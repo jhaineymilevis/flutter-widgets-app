0084ee74001d23372680021d77bf0c61d7a569b3
Jhainey Perez <jhainey.perez@dropi.co>
2024-10-26 14:23:12 -0500

    reating riverpod provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/config/routes/app_route.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'config/app_theme.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
--- lib/presentation/screens/counter_scree.dart
+++ lib/presentation/screens/counter_scree.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/presentation/providers/counter_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends ConsumerWidget {
   static const String name = "counter_screen";
   const CounterScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final clickCounter =
+        ref.watch(conunterProvider); //watichn counter provider of riverpod
     return Scaffold(
         appBar: AppBar(
           title: const Text("Counter Screen"),
         ),
         body: Center(
-          child:
-              Text("Valor:10", style: Theme.of(context).textTheme.titleLarge),
+          child: Text("Valor: $clickCounter",
+              style: Theme.of(context).textTheme.titleLarge),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
